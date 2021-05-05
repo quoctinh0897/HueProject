b3c99dfc232ebd1bbb5ebe5fb7a942f9936ef6c7
Tinh Tran <tinh.tran@navigosgroup.com>
2021-05-05 11:44:17 +0700

    Mapping port

--- deployments/start.sh
+++ deployments/start.sh
@@ -1,2 +1,4 @@
 #!/usr/bin/env sh
-uvicorn main:app --port 8080 --reload
+uvicorn main:app --port 8080 --host 0.0.0.0 --reload
+#docker build -t project/nginx -f deployments/Dockerfile ./ 
+#docker run -p 8080:8080 -it project/nginx 
